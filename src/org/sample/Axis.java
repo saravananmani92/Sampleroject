582160ab7f239a3111ab8f420075b59627489e0f
saravananmani92 <saranraja92@gmail.com>
2021-04-09 11:45:34 +0530

    sample project

---
+++ src/org/sample/Axis.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Axis{
+	public static void main(String[] args) {
+		int age=2;
+		if(age>=18) {
+			System.out.println("major");
+		}
+		else {
+			System.out.println("minor");
+		}
+	}
+	
+
+}
